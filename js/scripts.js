dec6cd6f4682155251ec13d284b6fc77c327e359
dennis-mwai <dennis.mwai55@gmail.com>
2019-05-06 11:43:34 +0300

    add scripts to fade out and fade in on hover

--- js/scripts.js
+++ js/scripts.js
@@ -37,6 +37,90 @@ $(document).ready(function (){
         });
     });
 
+    $(function(){
+        $('#work3').mouseover(function(){
+            $('#work3').fadeTo("slow", 0.15);
+            $('#work3text').show();
+            
+        });
+        $('#work3').mouseout(function(){
+            $('#work3').fadeTo("slow", 1);
+            $('#work3text').hide();
+        });
+    });
+
+    $(function(){
+        $('#work2').mouseover(function(){
+            $('#work2').fadeTo("slow", 0.15);
+            $('#work2text').show();
+            
+        });
+        $('#work2').mouseout(function(){
+            $('#work2').fadeTo("slow", 1);
+            $('#work2text').hide();
+        });
+    });
+
+    $(function(){
+        $('#work1').mouseover(function(){
+            $('#work1').fadeTo("slow", 0.15);
+            $('#work1text').show();
+            
+        });
+        $('#work1').mouseout(function(){
+            $('#work1').fadeTo("slow", 1);
+            $('#work1text').hide();
+        });
+    });
+
+    $(function(){
+        $('#work5').mouseover(function(){
+            $('#work5').fadeTo("slow", 0.15);
+            $('#work5text').show();
+            
+        });
+        $('#work5').mouseout(function(){
+            $('#work5').fadeTo("slow", 1);
+            $('#work5text').hide();
+        });
+    });
+
+    $(function(){
+        $('#work6').mouseover(function(){
+            $('#work6').fadeTo("slow", 0.15);
+            $('#work6text').show();
+            
+        });
+        $('#work6').mouseout(function(){
+            $('#work6').fadeTo("slow", 1);
+            $('#work6text').hide();
+        });
+    });
+
+    $(function(){
+        $('#work7').mouseover(function(){
+            $('#work7').fadeTo("slow", 0.15);
+            $('#work7text').show();
+            
+        });
+        $('#work7').mouseout(function(){
+            $('#work7').fadeTo("slow", 1);
+            $('#work7text').hide();
+        });
+    });
+
+    $(function(){
+        $('#work8').mouseover(function(){
+            $('#work8').fadeTo("slow", 0.15);
+            $('#work8text').show();
+            
+        });
+        $('#work8').mouseout(function(){
+            $('#work8').fadeTo("slow", 1);
+            $('#work8text').hide();
+        });
+    });
+
     $("#theform").submit(function(e){
         e.preventDefault();
         var name= $('#yourname').val();
